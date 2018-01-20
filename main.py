a8ae63e1bbd40dba7c7581cb37a709c72f7d7e25
rishikeshdevsot <rishikeshdevsot@gmail.com>
2018-01-20 14:44:56 -0500

    Converted to HSV and applied a white and yellow mask

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import cv2
 import numpy as np
-from matplotlib import pyplot as plt
+
+
+image = cv2.imread("img_3.jpg")
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+lower_yellow = np.array([20, 100, 100], dtype = "uint8")
+upper_yellow = np.array([30, 255, 255], dtype="uint8")
+img_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+mask_yellow = cv2.inRange(img_hsv, lower_yellow, upper_yellow)
+mask_white = cv2.inRange(gray_image, 200, 255)
+mask_yw = cv2.bitwise_or(mask_white, mask_yellow)
+mask_yw_image = cv2.bitwise_and(gray_image, mask_yw)
+cv2.imshow('image', mask_yw_image)
+cv2.waitKey(0)
